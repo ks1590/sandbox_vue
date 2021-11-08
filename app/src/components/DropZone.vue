ea517cee38c4f3fabc7721b833bcd81cc9beb9f1
kensuke <35k.0101@gmail.com>
2021-11-08 22:08:43 +0900

    drop zoneのレイアウト作成

--- app/src/components/DropZone.vue
+++ app/src/components/DropZone.vue
@@ -1,5 +1,10 @@
 <template>
-  <div></div>
+  <div class="dropzone">
+    <span>Drag or Drop File</span>
+    <span>OR</span>
+    <label for="dropzoneFile">Select File</label>
+    <input type="file" id="dropzoneFile">
+  </div>
 </template>
 
 <script>
@@ -8,4 +13,28 @@ export default {
 }
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+  .dropzone {
+    width: 400px;
+    height: 200px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    row-gap: 16px;
+    border: 2px dashed #41b883;
+    background: #fff;
+    transition: 0.3s ease all;
+  }
+
+  label {
+    padding: 8px 12px;
+    color: #fff;
+    background: #41b883;
+    transition: 0.3s ease all;
+  }
+
+  input {
+    display: none;
+  }
+</style>
