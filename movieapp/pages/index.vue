c3f592f8d327412f0fb967a14326e4d94b5b0001
ks1590 <69412467+ks1590@users.noreply.github.com>
2021-12-19 21:47:28 +0900

    WIP:add movie content

--- movieapp/pages/index.vue
+++ movieapp/pages/index.vue
@@ -1,11 +1,49 @@
 <template>
   <div class="home">
     <Hero />
+
+    <div class="container movies">
+      <div id="movie-greid" class="movie-grid">
+        <div class="movie" v-for="movie in movies" :key="movie.id">
+          <div class="movie-img">
+            <img :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`" alt="">
+            <p class="review">{{ movie.vote_average }}</p>
+            <p class="overview">{{ movie.overview }}</p>
+          </div>
+          <div class="info">
+            <p class="title">{{ movie.title.slice(0, 25) }}
+              <span v-if="movie.title.length > 25">...</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'IndexPage',
+  data() {
+    return {
+      movies: [],
+    }
+  },
+  async fetch() {
+    await this.getMovies();
+  },
+  methods: {
+    async getMovies() {
+      const data = axios.get(
+        'https://api.themoviedb.org/3/movie/now_playing?api_key=37ed43a4f8eaa2abd75f9283692947bc&language=en-US&page=1'  
+      );
+      const result = await data;
+      result.data.results.forEach(movie => {
+        this.movies.push(movie);
+      });
+      console.log(result.data.results);
+    },
+  },
 }
 </script>
